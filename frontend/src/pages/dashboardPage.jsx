905230952466cd66cc25342992d5c763fe6bbaa1
Ayush Auti <ayushauti5605@gmail.com>
2025-11-26 23:21:56 +0530

    hovering effect added at background

--- frontend/src/pages/dashboardPage.jsx
+++ frontend/src/pages/dashboardPage.jsx
@@ -4,7 +4,7 @@ export default function DashboardPage(){
 
      return (
     <div className="relative flex h-screen w-full overflow-hidden">
-         <Boxes className="absolute inset-0 opacity-[0.50] pointer-events-none" />
+         <Boxes className="absolute inset-0 opacity-[0.50] pointer-events-auto" />
         <div className="relative z-10 p-6">
       <h1 className="text-3xl font-bold text-neutral-50">Dashboard</h1>
       <p className="text-neutral-300 mt-2">Welcome to your Finance Dashboard.</p>
