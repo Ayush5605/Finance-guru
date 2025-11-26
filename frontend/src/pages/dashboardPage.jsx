01e798dcb1e0e27f16373bb444981e18f5645eca
Ayush Auti <ayushauti5605@gmail.com>
2025-11-26 23:19:57 +0530

    changes made in dashbard

--- frontend/src/pages/dashboardPage.jsx
+++ frontend/src/pages/dashboardPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BackgroundBeamsWithCollision } from "../components/ui/background-beams-with-collision";
+import { Boxes } from "../components/ui/background-boxes";
 export default function DashboardPage(){
 
      return (
-    <div className="w-full h-full">
-         <BackgroundBeamsWithCollision className="absolute inset-0 opacity-[0.50] pointer-events-none" />
+    <div className="relative flex h-screen w-full overflow-hidden">
+         <Boxes className="absolute inset-0 opacity-[0.50] pointer-events-none" />
         <div className="relative z-10 p-6">
       <h1 className="text-3xl font-bold text-neutral-50">Dashboard</h1>
       <p className="text-neutral-300 mt-2">Welcome to your Finance Dashboard.</p>
