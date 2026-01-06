5fcd6380896193083e73383cea1ee46a0f861ee8
Ayush Auti <ayushauti5605@gmail.com>
2026-01-06 22:36:44 +0530

    premium logic

--- frontend/src/components/chatBot/chatBot.jsx
+++ frontend/src/components/chatBot/chatBot.jsx
@@ -1,15 +1,31 @@
 // src/components/chatbot/ChatBot.jsx
-import { useState } from "react";
+import { useContext, useState } from "react";
 import ChatButton from "./ChatButton";
 import ChatWindow from "./ChatWindow";
+import {useUser} from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 export default function ChatBot() {
   const [open, setOpen] = useState(false);
+  const{isPremium,loading}=useUser();
+  const navigate=useNavigate();
+
+
+  const handleClick=()=>{
+    if(loading)return;
+
+    if(!isPremium){
+      navigate("/premium");
+      return;
+    }
+
+    setOpen(true);
+  }
 
   return (
     <>
       {open && <ChatWindow onClose={() => setOpen(false)} />}
-      <ChatButton onClick={() => setOpen(true)} />
+      <ChatButton onClick={handleClick} />
     </>
   );
 }
